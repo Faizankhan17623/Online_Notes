b7f2804385f4e5305e1b5b6afee51f2495a7f535
FAIZAN KHAN <faizankhan901152@gmail.com>
2026-03-26 17:42:20 +0530

    chagnes done in the server bookmark

--- server/models/Bookmark.js
+++ server/models/Bookmark.js
@@ -5,9 +5,10 @@ const bookmarkSchema = new mongoose.Schema({
   url: { type: String, required: true, trim: true },
   description: { type: String, trim: true, default: '' },
   tags: { type: [String], default: [] },
-  sectionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Section', required: true },
-  subsectionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Subsection', required: true },
+  sectionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Section' },
+  subsectionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Subsection' },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  direct: { type: Boolean, default: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Bookmark', bookmarkSchema);
